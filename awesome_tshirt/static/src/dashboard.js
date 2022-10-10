95f78e935ce28f34209de5a9ffefbc2fe67e2610
BT-rmartin <raul.martin@bt-group.com>
2022-10-10 16:06:52 +0200

    exercise 2.1. layout

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -4,8 +4,10 @@ import { registry } from "@web/core/registry";
 import { Counter } from "@awesome_tshirt/counter/counter";
 import { Todo } from "@awesome_tshirt/todo/todo";
 import { Card } from "@awesome_tshirt/card/card";
+import { Layout } from "@web/search/layout";
+import { getDefaultConfig } from "@web/views/view";
 
-const { Component, useState, onMounted, useRef } = owl;
+const { Component, useState, onMounted, useRef, useSubEnv} = owl;
 
 class AwesomeDashboard extends Component {
       setup() {
@@ -17,6 +19,16 @@ class AwesomeDashboard extends Component {
           // so as soon as the page is reloaded the focus is there
           const ref = useRef('todoInput')
           onMounted(() => ref.el && ref.el.focus());
+
+          useSubEnv({
+                config: {
+                    ...getDefaultConfig(),
+                    ...this.env.config,
+                },
+            });
+            this.display = {
+                controlPanel: { "top-right": false, "bottom-right": false },
+            };
       }
 
       addTodo(ev){
@@ -40,6 +52,6 @@ class AwesomeDashboard extends Component {
 }
 
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
-AwesomeDashboard.components = { Counter, Todo, Card };
+AwesomeDashboard.components = { Counter, Todo, Card, Layout};
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
