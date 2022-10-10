a8746d8fff92f918789079dea8cdd95a7339169b
BT-rmartin <raul.martin@bt-group.com>
2022-10-10 11:04:15 +0200

    exercise 1.2

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -2,13 +2,17 @@
 
 import { registry } from "@web/core/registry";
 import { Counter } from "@awesome_tshirt/counter/counter";
+import { Todo } from "@awesome_tshirt/todo/todo";
 
 const { Component, useState } = owl;
 
 class AwesomeDashboard extends Component {
+      setup() {
+        this.todo = { id: 3, description: "buy milk", done: false };
+      }
 }
 
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
-AwesomeDashboard.components = { Counter };
+AwesomeDashboard.components = { Counter, Todo };
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
---
+++ awesome_tshirt/static/src/todo/todo.js
@@ -0,0 +1,9 @@
+/** @odoo-module **/
+
+const { Component, useState } = owl;
+
+
+export class Todo extends Component {
+}
+
+Todo.template = "awesome_tshirt.todo_view";
