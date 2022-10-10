ab2a4ada7b7f564a61ce22976ad5bbb6afeb4112
BT-rmartin <raul.martin@bt-group.com>
2022-10-10 10:36:15 +0200

    First exercise

---
+++ awesome_tshirt/static/src/counter/counter.js
@@ -0,0 +1,17 @@
+/** @odoo-module **/
+
+const { Component, useState } = owl;
+
+
+export class Counter extends Component {
+    setup() {
+        this.state = useState({value: 1});
+    }
+
+    increment() {
+        this.state.value = this.state.value + 1;
+    }
+
+}
+
+Counter.template = "awesome_tshirt.counter_view";
