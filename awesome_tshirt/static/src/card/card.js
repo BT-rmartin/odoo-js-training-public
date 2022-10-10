36658eb44f9017bebc3d1bbf2830eb885a08975c
BT-rmartin <raul.martin@bt-group.com>
2022-10-10 15:00:37 +0200

    exercise 1.9 t-slot

---
+++ awesome_tshirt/static/src/card/card.js
@@ -0,0 +1,10 @@
+/** @odoo-module **/
+
+const { Component, useState } = owl;
+
+
+export class Card extends Component {
+
+}
+
+Card.template = "awesome_tshirt.card_view";
--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -3,6 +3,7 @@
 import { registry } from "@web/core/registry";
 import { Counter } from "@awesome_tshirt/counter/counter";
 import { Todo } from "@awesome_tshirt/todo/todo";
+import { Card } from "@awesome_tshirt/card/card";
 
 const { Component, useState, onMounted, useRef } = owl;
 
@@ -39,6 +40,6 @@ class AwesomeDashboard extends Component {
 }
 
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
-AwesomeDashboard.components = { Counter, Todo };
+AwesomeDashboard.components = { Counter, Todo, Card };
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
