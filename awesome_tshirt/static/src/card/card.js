ed68fe45b3052f7629b8c6cad2f3932113499684
BT-rmartin <raul.martin@bt-group.com>
2022-10-10 15:37:04 +0200

    exercise 1.10 validation t-slot

--- awesome_tshirt/static/src/card/card.js
+++ awesome_tshirt/static/src/card/card.js
@@ -8,3 +8,12 @@ export class Card extends Component {
 }
 
 Card.template = "awesome_tshirt.card_view";
+Card.props = {
+    slots: {
+        type: Object,
+        shape: {
+            default: Object,
+            title: {type: Object, optional: true}
+        },
+    },
+};
