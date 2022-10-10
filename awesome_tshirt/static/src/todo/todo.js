60108d8cec82e468ef77696d426a48c0028cbd2c
BT-rmartin <raul.martin@bt-group.com>
2022-10-10 11:43:00 +0200

    exercise 1.3. failing

--- awesome_tshirt/static/src/todo/todo.js
+++ awesome_tshirt/static/src/todo/todo.js
@@ -4,6 +4,13 @@ const { Component, useState } = owl;
 
 
 export class Todo extends Component {
+
 }
 
 Todo.template = "awesome_tshirt.todo_view";
+Todo.props = {
+            id: Number,
+            description: String,
+            done: Boolean,
+
+     };
